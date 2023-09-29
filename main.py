33b88c9c35a918b87baef749dedf257663ac885f
SebastienIbrahim <sebasuchienu@hotmail.fr>
2023-09-29 20:22:05 +0200

    Update main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from scrapers.scraper import SiteScraper
 from pathlib import Path
 from utils.helpers import load_config_file
 from matching_bookmakers import find_similar_matches
+import time
 from find_surebet.countries_matching import (
     get_matched_countries,
     get_data_from_matched_countries_per_site,
@@ -27,10 +28,21 @@ def main(sites_name, raw_data_path="instance"):
     comptetions_data = get_data_from_matched_countries_per_site(
         matched_countries, not_matched_countries
     )
+    data_list = [
+        item
+        for item in comptetions_data
+        if "matches" in item and all(match.get("teams") for match in item["matches"])
+    ]
+
     similarity_threshold = settings["similarity_threshold"]
-    similar_matches = find_similar_matches(comptetions_data, similarity_threshold)
+    t0 = time.time()
+    similar_matches = find_similar_matches(data_list, similarity_threshold)
+    t1 = time.time()
+    print(f"Time to find similar matches: {(t1-t0)/60} minutes")
+    len(similar_matches.keys())
     for match in similar_matches.values():
         data_matches.update(match)
+    len(data_matches.keys())
     opportunities = find_surbets_opportunities(
         data_matches, investissement_amount=100, nb_way=3, draw_position=1
     )
